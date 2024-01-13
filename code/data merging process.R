5ec2438f8738198b375762206bd93b5dd37a09b4
rapkroes <150437338+rapkroes@users.noreply.github.com>
2024-01-13 23:03:04 +0100

    update 13.01. 23.02

--- code/data merging process.R
+++ code/data merging process.R
@@ -2,10 +2,16 @@
 
 ###load the rest of the (publicly available) data
 
-location.name<- c("aalen","ammerbuch","baiersbronn","boeblingen","boennigheim","pforzheim","pleidelsheim","salach","schluchsee","schopfheim","schwaebisch gmuend","waldachtal","wendlingen")
+location.name<- c("aalen","ammerbuch","baiersbronn","boeblingen","boennigheim","pforzheim","pleidelsheim","salach","schluchsee","schopfheim","schwaebisch_gmuend","waldachtal","wendlingen")
 
 #weather data
-wetter_aalen<- read.csv("https://raw.githubusercontent.com/rapkroes/impact-of-ambient-temperature-on-visits-to-general-practitioners/main/weather%20data/wetter%20aalen.csv")
+for(i in seq_along(location.name)){
+  url_string<- paste0("https://raw.githubusercontent.com/rapkroes/impact-of-ambient-temperature-on-visits-to-general-practitioners/main/weather%20data/wetter%20",location.name[i],".csv")
+  im<- read.csv(url_string)
+  assign(paste0("wetter_",location.name[i]),im)
+}
+
+#daylight data
 for(i in seq_along(location.name)){
   url_string<- paste0("https://raw.githubusercontent.com/rapkroes/impact-of-ambient-temperature-on-visits-to-general-practitioners/main/weather%20data/wetter%20",location.name[i],".csv")
   im<- read.csv(url_string)
