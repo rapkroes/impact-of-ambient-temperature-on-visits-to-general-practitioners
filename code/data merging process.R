4482d9b282becbed8bb9d22b6867ca57f8900d07
rapkroes <150437338+rapkroes@users.noreply.github.com>
2024-01-13 20:02:23 +0100

    update 13.01. 20.02

--- code/data merging process.R
+++ code/data merging process.R
@@ -1,5 +1,5 @@
 ###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###Data Merging Process###
 
-#load the rest of the (publicly available) data
-
-
+###load the rest of the (publicly available) data
+#weather data
+wetter_aalen<- read.csv("https://github.com/rapkroes/impact-of-ambient-temperature-on-visits-to-general-practitioners/blob/5380bbfc9a623191de5e8f963d87cfb799d48932/weather%20data/wetter%20aalen.csv")
