b9ea2b2b8aa470e5ce5a6f33e8887928523376d7
rapkroes <150437338+rapkroes@users.noreply.github.com>
2024-04-16 04:00:09 +0200

    update 16.04 4.00

--- code/output eval.R
+++ code/output eval.R
@@ -9,7 +9,6 @@ booster.eval<- function(booster, inputdf, no.draws, eval.var, eval.seq, seed, ev
   # eval.seq is the vector of different values that are to be evaluated (the different values "X" may take)
   # seed is the random seed
   # eval.type prediction output type. Should be "response". For further details, check https://lightgbm.readthedocs.io/en/latest/R/reference/predict.lgb.Booster.html
-  browser()
   library(lightgbm)
   set.seed(seed)
   initial.data<- inputdf[sample(seq(1,nrow(inputdf)),no.draws,replace = FALSE),]
@@ -17,7 +16,7 @@ booster.eval<- function(booster, inputdf, no.draws, eval.var, eval.seq, seed, ev
   eval.data<- as.data.frame(matrix(NA, nrow = no.draws * length(eval.seq),
                                    ncol = ncol(inputdf)))
   colnames(eval.data)<- colnames(inputdf)
-  eval.var.col<- which(colnames(eval.var)==eval.var)
+  eval.var.col<- which(colnames(eval.data)==eval.var)
   for(i in seq_along(eval.seq)){
     sel.rows<- seq(1, no.draws) + (i-1) * no.draws
     eval.data[sel.rows,]<- initial.data
@@ -32,10 +31,66 @@ booster.eval<- function(booster, inputdf, no.draws, eval.var, eval.seq, seed, ev
   out<- list()
   for(i in seq(1,no.draws)){
     selection<- seq(1, nrow(eval.data), by = no.draws)
-    out[[i]]<- cbind(eval.data[selection,], prediction.output[selection,])
+    im<- cbind(eval.data[selection, eval.var.col], prediction.output[selection,])
+    colnames(im)<- c(eval.var, paste(rep("V", ncol(prediction.output)), 
+                                     seq(1, ncol(prediction.output)), 
+                                     sep = "_"))
+    out[[i]]<- im
   }
-  colnames(out)<- c(eval.var, paste(rep("V", ncol(prediction.output)), 
-                                    seq(1, ncol(prediction.output))))
   return(out)
 }
 
+x<- booster.eval(booster = model_q2_TDI, inputdf = df_qx(di = "TDI", q = 2), 
+             no.draws = 100, eval.var = "age", eval.seq = seq(18,30),
+             seed = 18521)
+
+eval.plot<- function(eval.list){
+  browser()
+  
+  no.draws<- length(eval.list)
+  x.seq<- eval.list[[1]][,1]
+  range_x<- range(x.seq)
+  no.y<- ncol(eval.list[[1]])-1
+  
+  for(i in seq(1, no.y)){
+    new.df<- as.data.frame(matrix(NA, nrow = length(x.seq), ncol = no.y + 1))
+    new.df[,1]<- x.seq
+    for(j in seq(1, no.draws)){
+      new.df[,j + 1]<- eval.list[[j]][,i + 1]
+    }
+    colnames(new.df)<- c("x", paste(rep("P", no.y), seq(1, no.y), sep = "_"))
+    p<- ggplot(data = new.df)
+    for(j in seq(1, no.draws)){
+      p<- p + geom_line(data = eval.list[[j]], aes_string(x = x, y = paste0("P_",j)), col = j)
+    }
+    filename <- paste0('plot_', i, '.png')
+    ggsave(filename, plot = p, device = "png")
+  }
+}
+eval.plot(x)
+library(ggplot2)
+
+#ChatGPT
+save_plots <- function(eval_list) {
+  browser()
+  k <- length(eval_list)
+  
+  # Loop through each plot (p - 1 plots)
+  for (i in 1:(ncol(eval_list[[1]]) - 1)) {
+    # Create a new ggplot object
+    p <- ggplot()
+    
+    # Add lines for each element in eval_list
+    for (j in 1:k) {
+      p <- p + geom_line(data = eval_list[[j]], aes_string(x = names(eval_list[[j]])[1], y = names(eval_list[[j]])[i + 1]), col = j)
+    }
+    
+    # Customize the plot
+    p <- p + labs(x = 'X', y = paste('Y', i, sep = ''), title = paste('Plot', i))
+    
+    # Save the plot to working directory
+    filename <- paste0('plot_', i, '.png')
+    ggsave(filename, plot = p, device = "png")
+  }
+}
+save_plots(x)
