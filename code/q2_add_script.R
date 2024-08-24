a11762d8c40cf1aa1340125ba0d2de98352249ff
rapkroes <150437338+rapkroes@users.noreply.github.com>
2024-08-24 03:29:47 +0200

    update 24.08 03.29

---
+++ code/q2_add_script.R
@@ -0,0 +1,18 @@
+###additional script
+
+#lade folgende Datei (Datei öffnen): neuste Version von ws_[Datum]_new.RData im Ordner N:/StudentischeHilfskraefte/_Kroes (Christoph)/Routinedaten/03_Hitze/R
+
+perf.q2_TDI
+perf.q2_HW
+perf.q2_SDI
+sum(full.df_7$diag_class==1)
+sum(full.df_7$diag_class==2)
+sum(full.df_7$diag_class==3)
+sum(full.df_7$diag_class==4)
+sum(full.df_7$diag_class==5)
+sum(full.df_7$diag_class==6)
+sum(full.df_7$diag_class==7)
+sum(full.df_7$diag_class==8)
+sum(full.df_7$diag_class==9)
+sum(full.df_7$diag_class==10)
+sum(full.df_7$diag_class==11)
